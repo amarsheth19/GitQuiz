ecda7dbf599a4129a0ec683f21e79c69245af3b4
amarsheth19 <amar.sheth19@gmail.com>
2023-12-07 11:53:41 -0500

    Second Commit
    added lambda expression

--- Class.java
+++ Class.java
@@ -3,6 +3,22 @@ interface Add{
 }
 public class Class {
     public static void main(String[] args){
+        int firstNumber =0;
+        int lastNumber = 0;
+        for(int i = 0; i <args.length; i++)
+            System.out.println(args[i]);
+
+
+        Add a = (int firstNum, int lastNum) -> {
+            int amount = 0;
+            for(int i = firstNumber; i<=lastNumber; i++)
+                amount+=i;
+            return amount;
+        };
+
+        int answer = a.calculate(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+
+        System.out.println("Amount" + answer);
 
     }
 
